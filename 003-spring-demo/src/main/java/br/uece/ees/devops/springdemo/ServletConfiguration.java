223d1850a7a46a05a847a02387082041ff205857
Wellington Pereira <wellington.costa128@gmail.com>
2024-02-26 12:17:04 -0300

    Remove @ApplicationScope usage

--- 003-spring-demo/src/main/java/br/uece/ees/devops/springdemo/ServletConfiguration.java
+++ 003-spring-demo/src/main/java/br/uece/ees/devops/springdemo/ServletConfiguration.java
@@ -3,11 +3,10 @@ package br.uece.ees.devops.springdemo;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.annotation.ApplicationScope;
 
 @Configuration
 public class ServletConfiguration {
-    @Bean @ApplicationScope
+    @Bean
     public ServletRegistrationBean<HelloWorldServlet> customServletBean() {
         return new ServletRegistrationBean<>(new HelloWorldServlet(), "/servlet");
     }
