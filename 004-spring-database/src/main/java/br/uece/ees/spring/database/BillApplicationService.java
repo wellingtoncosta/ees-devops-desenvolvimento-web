82a8f9683bfeb3398c2458a14553fccece34761d
Wellington Pereira <wellington.costa128@gmail.com>
2024-02-29 21:18:13 -0300

    Implement an application service to decouple application logic from controller

---
+++ 004-spring-database/src/main/java/br/uece/ees/spring/database/BillApplicationService.java
@@ -0,0 +1,28 @@
+package br.uece.ees.spring.database;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class BillApplicationService {
+
+    private final BillRepository repository;
+
+    public BillApplicationService(BillRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<BillDto> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(BillDto::from)
+                .collect(Collectors.toList());
+    }
+
+    public BillDto save(BillDto dto) {
+        var created = repository.save(dto.toEntity());
+        return BillDto.from(created);
+    }
+}
--- 004-spring-database/src/main/java/br/uece/ees/spring/database/BillController.java
+++ 004-spring-database/src/main/java/br/uece/ees/spring/database/BillController.java
@@ -1,35 +1,34 @@
 package br.uece.ees.spring.database;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/bills")
 public class BillController {
 
-    private final BillRepository repository;
+    private final BillApplicationService applicationService;
 
-    public BillController(BillRepository repository) {
-        this.repository = repository;
+    public BillController(BillApplicationService applicationService) {
+        this.applicationService = applicationService;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<BillDto> getAll() {
-        return repository.findAll()
-                .stream()
-                .map(BillDto::from)
-                .collect(Collectors.toList());
+    public ResponseEntity<List<BillDto>> getAll() {
+        return ResponseEntity.ok(applicationService.findAll());
     }
 
     @PostMapping(
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
-    public BillDto save(@RequestBody BillDto dto) {
-        var created = repository.save(dto.toEntity());
-        return BillDto.from(created);
+    public ResponseEntity<BillDto> save(@RequestBody BillDto dto) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(applicationService.save(dto));
     }
 }
