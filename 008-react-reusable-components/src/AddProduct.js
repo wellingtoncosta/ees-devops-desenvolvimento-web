4cb9a81fa56187ac1c4c079ad0ca57357367710d
Wellington Pereira <wellington.costa128@gmail.com>
2024-03-22 09:20:20 -0300

    refactor: break AddProducts into small components

--- 008-react-reusable-components/src/AddProduct.js
+++ 008-react-reusable-components/src/AddProduct.js
@@ -2,48 +2,78 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { products } from './products.js';
 
-function AddProduct() {
-    const navigate = useNavigate()
-    const [name, setName] = useState('')
-    const [price, setPrice] = useState('')
-    const [category, setCategory] = useState('')
-    
+function Form({ name, onNameChanged, price, onPriceChanged, category, onCategoryChanged }) {
     return (
         <div>
             <label>
                 Name:
                 {' '}
-                <input value={name} onChange={(e) => setName(e.target.value)} />
+                <input value={name} onChange={(e) => onNameChanged(e.target.value)} />
             </label>
             <br/>
             <br/>
             <label>
                 Price:
                 {' '}
-                <input value={price} onChange={(e) => setPrice(e.target.value)} />
+                <input value={price} onChange={(e) => onPriceChanged(e.target.value)} />
             </label>
             <br/>
             <br/>
             <label>
                 Category:
                 {' '}
-                <input value={category} onChange={(e) => setCategory(e.target.value)} />
+                <input value={category} onChange={(e) => onCategoryChanged(e.target.value)} />
             </label>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+        </div>
+    )
+}
+
+function Buttons({name, price, category, navigate}) {
+    return (
+        <div>
             <button onClick={(e) => {
                 products.push({
                     name: name,
                     price: price,
                     category: category
                 })
-                
+
                 navigate('/')
             }}>Save</button>
             {' '}
             <button onClick={(e) => navigate('/')}>Back</button>
+        </div>
+    )
+}
+
+function AddProduct() {
+    const navigate = useNavigate()
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState('')
+    const [category, setCategory] = useState('')
+
+    return (
+        <div>
+            <Form
+                name={name}
+                onNameChanged={name => setName(name)}
+                price={price}
+                onPriceChanged={price => setPrice(price)}
+                category={category}
+                onCategoryChanged={category => setCategory(category)}
+            />
+
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+            <Buttons
+                name={name}
+                price={price}
+                category={category}
+                navigate={navigate}
+            />
         </div>
     )
 }
